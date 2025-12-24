414d3c660cd1423e0ec0c29b6ff350651af79de7
Morgan Swanson <hello@morganswanson.com>
2025-12-24 00:41:20 -0800

    revert: remove env config (use Amplify Secrets instead)
    - Environment variables are build-time only in Amplify
    - Runtime values need Amplify Secrets management
    - User will configure DATABASE_URL, NEXTAUTH_SECRET, NEXTAUTH_URL as secrets

--- next.config.js
+++ next.config.js
@@ -6,11 +6,6 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
 }
 
 module.exports = nextConfig
